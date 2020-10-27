5f9b1a9288a80f5b2c375eac9fdff5ae7ca4efe4
Felipe Ferreira <felipe.silfer@gmail.com>
2020-10-27 16:21:29 -0300

    Adição da página de pagamentos.

---
+++ js/pagseguro.js
@@ -0,0 +1,31 @@
+function spoilerBtt(typeBtt) {
+
+	spoiler = document.getElementsByClassName('spoiler');
+
+		for (i = 0; i < spoiler.length; i++) {
+
+		if (spoiler[i].id == typeBtt) {
+			spoiler[i].style.display = 'inline';
+		} 
+		else { 
+			spoiler[i].style.display = 'none';
+		}
+
+		if (document.getElementsByClassName('typeBtt')[i].id == 'btt'+typeBtt){
+				document.getElementById('btt'+typeBtt).disabled = true;
+			}
+			else{
+				document.getElementsByClassName('typeBtt')[i].disabled = false;
+			}
+	}
+
+}
+
+function goToNewPage(dropdownlist)
+ {
+ var url = dropdownlist.options[dropdownlist.selectedIndex].value;
+ if (url != "")
+ {
+ window.open(url);
+ }
+ }
